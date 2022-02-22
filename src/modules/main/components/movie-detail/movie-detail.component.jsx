7d5223badee6f1ffa147a5b51d250640f3b2af0f
Tảo <ngobaophuongtao@gmail.com>
2022-02-22 12:40:26 +0700

    deploy to host

--- src/modules/main/components/movie-detail/movie-detail.component.jsx
+++ src/modules/main/components/movie-detail/movie-detail.component.jsx
@@ -4,11 +4,15 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getMovieDetailAction } from '../../../../store/actions/movie.actions';
 import PopUpTrailer from '../pop-up-trailer/pop-up-trailer.component';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import './movie-detail.component.scss';
 
 function MovieDetail() {
   const trailer = useSelector((state) => state.movie.movieDetail);
   const dispatch = useDispatch();
   const { id } = useParams();
+  console.log(trailer);
 
   const newMovieDetail = { trailer };
 
@@ -29,14 +33,23 @@ function MovieDetail() {
   window.addEventListener('resize', handleRenderSideBar);
 
   return (
-    <section className=' container'>
+    <section className=' container movieDetailContain'>
       <div className='row'>
-        <div className='col-md-8'>
-          <div>left</div>
-          <div>right</div>
+        <div className='col-md-8 row'>
+          <div className='col-lg-4 movieDetailPicContain'>
+            <div className='btnPicBox'>
+              <img src={trailer.hinhAnh} alt='movieDetail' />
+              <button onClick={() => handleTrailer(true)}>
+                <FontAwesomeIcon icon={solid('play')} />
+              </button>
+            </div>
+          </div>
+
+          <div className='col-lg-8 movieInfo'>
+            <h2>{trailer.tenPhim}</h2>
+          </div>
         </div>
         {windowWidth >= 768 ? <div className='col-md-4'>sidebar</div> : ''}
-        {/* <div className='col-md-4'>sidebar</div> */}
       </div>
       <PopUpTrailer
         onClick={() => handleTrailer(false)}
